ce8afc2a3d7f9e05c8205a4a4ee6c3032a416384
micaelakorol <micaelakorol@gmail.com>
2022-11-08 15:06:57 -0300

    bug fix contact

--- src/firebase/credentials.js
+++ src/firebase/credentials.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
+import { apiKey, appId, auth, messagingSenderId, proyectId, storageBucket } from "../api_key";
 
 const credentials = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: apiKey,
+  authDomain: auth,
+  projectId: proyectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId
 };
 
 export const app = initializeApp(credentials);
